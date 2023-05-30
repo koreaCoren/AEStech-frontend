3a9282b0333b7ff315cf3b5093505982773f1812
jung woo Kwon <kwog5050@naver.com>
2023-05-30 14:11:19 +0900

    modified:   README.md 	modified:   src/pages/main/Main.js

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Loading from 'components/loding/Loading';
-
 import * as Style from "assets/styleComponent/main/main"
 
 const Main = () => {
@@ -12,7 +10,7 @@ const Main = () => {
                 <h2>에너지 절감 장치의 확실한 행복</h2>
                 <h3>AES테크는 고효율 에너지 절감 장치로 지속가능한 일상을 만들어가겠습니다.</h3>
 
-                ㅕㅣ
+
             </div>
         </main>
     );
