0a644117984c4f6c5cc7885dd5cfa7e4552cdd5a
jung woo Kwon <kwog5050@naver.com>
2023-05-31 10:45:29 +0900

    1

--- src/assets/styleComponent/product/common.js
+++ src/assets/styleComponent/product/common.js
@@ -2,4 +2,19 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     padding: 150px 0px;
+    .list{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-gap: 15px;
+        text-align: center;
+        margin-top: 30px;
+    }
+    .list li{}
+    .list li a > div{
+        position: relative;
+    }
+    .list li a > div > div{
+        position: absolute;
+        display: none;
+    }
 ` 
--- src/pages/sub/product/Product_01.js
+++ src/pages/sub/product/Product_01.js
@@ -1,11 +1,79 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Tap from 'components/product/Tap';
 
 import * as Style from "assets/styleComponent/product/common"
 
+import product01 from "assets/images/prodcut/1.png";
+import product03 from "assets/images/prodcut/3.png";
+import product04 from "assets/images/prodcut/4.png";
+import product05 from "assets/images/prodcut/5.png";
+import product06 from "assets/images/prodcut/6.png";
+
 const Product_01 = () => {
     return (
-        <Style.Container>ss
+        <Style.Container>
+            <div className="wrap">
+                <Tap />
 
+                <ul className='list'>
+                    <li>
+                        <Link>
+                            <div>
+                                <img src={product01} alt="" />
+                                <div className="more">자세히보기</div>
+                            </div>
+                            <h5>■ 냉풍건조기 [CWCD-230D]</h5>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link>
+                            <div>
+                                <img src={product01} alt="" />
+                                <div className="more">자세히보기</div>
+                            </div>
+                            <h5>■ 냉풍건조기 [CWCD-230D]</h5>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link>
+                            <div>
+                                <img src={product03} alt="" />
+                                <div className="more">자세히보기</div>
+                            </div>
+                            <h5>■ 냉풍건조기 [CWCD-230D]</h5>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link>
+                            <div>
+                                <img src={product04} alt="" />
+                                <div className="more">자세히보기</div>
+                            </div>
+                            <h5>■ 냉풍건조기 [CWCD-230D]</h5>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link>
+                            <div>
+                                <img src={product05} alt="" />
+                                <div className="more">자세히보기</div>
+                            </div>
+                            <h5>■ 냉풍건조기 [CWCD-230D]</h5>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link>
+                            <div>
+                                <img src={product06} alt="" />
+                                <div className="more">자세히보기</div>
+                            </div>
+                            <h5>■ 냉풍건조기 [CWCD-230D]</h5>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </Style.Container>
     );
 };
